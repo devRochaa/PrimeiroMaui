1dc9037eeba9cba230e95af7c28d9d9a37ff6b1f
Usuario <Usuario@CPD03-M10>
2024-02-16 14:31:37 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PrimeiroMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PrimeiroMaui/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace PrimeiroMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            double n1 = Convert.ToDouble(txt_n1.Text);
+            double n2 = Convert.ToDouble(txt_n2.Text);
+
+            double res = n1 + n2;
+
+            string msg = $"O resultado é: "+ res;
+
+            DisplayAlert("Maui", msg, "Fechar");
+        }
+    }
+
+}
